ae0ae653133f78ee1f2882169e263bae56ec8d2b
my name <blueshaperd3@gmail.com>
2024-03-20 23:10:59 +0500

    not done

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ var val1 =prompt ("Enter your first value")
 var val2 =prompt ("Enter your second value")
 var sign =prompt ("Enter your opprator")
 console.log(val1+sign+val2)
+if(sign === "+"){
+    alert(val1+sign+val2)
+}else if(sign === "-"){
+    alert(val1-sign-val2)
+}else if(sign === "*"){
+    alert(val1*sign*val2)
+}else if(sign === "/"){
+    alert(val1/sign/val2)
+}
