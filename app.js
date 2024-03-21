527899c818a702aa3c769070c3ae4ec6462280c9
my name <blueshaperd3@gmail.com>
2024-03-21 23:46:08 +0500

    equation not done

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-var val1 =prompt ("Enter your first value")
-var val2 =prompt ("Enter your second value")
-var sign =prompt ("Enter your opprator")
-console.log(val1+sign+val2)
-if(sign === "+"){
-    alert(val1+sign+val2)
-}else if(sign === "-"){
-    alert(val1-sign-val2)
-}else if(sign === "*"){
-    alert(val1*sign*val2)
-}else if(sign === "/"){
-    alert(val1/sign/val2)
-}
+// Simple Calculater
+
+// var val1 =prompt ("Enter your first value")
+// var sign =prompt ("Enter your opprator")
+// var val2 =prompt ("Enter your second value")
+
+// console.log(val1+sign+val2)
+// if(sign === "+"){
+//     alert((+val1)+(+val2))
+// }else if(sign === "-"){
+//     alert(val1-val2)
+// }else if(sign === "*"){
+//     alert(val1*val2)
+// }else if(sign === "/"){
+//     alert(val1/val2)
+// }else if(sign === "%"){
+//     alert(val1/val2*100 + "%")
+// }
+
+
+// Simple equation calculater
+
+var eqa =  prompt("Please enter an equation");
+var num = eqa.length
+for (var i=0; i<num; i++)
+if ()
